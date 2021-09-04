fe319e4d69d0bd93ce1aa2c7c431e8b2f1256004
esfernandezb <barbielen@hotmail.com>
2021-09-04 12:14:01 -0500

    verificar el token

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,4 @@
-import * as authJwt from "./authJwt";
-import * as verifySignup from "./verifySignup";
+import{verifyToken} from "./authJwt";
 
-export { authJwt, verifySignup };
+
+export { verifyToken };
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,13 +1,13 @@
-import {Router} from 'express'
-const router = Router()
-import * as productsCtrl from '../controllers/products.controller'
+import {Router} from 'express';
+const router = Router();
+import * as productsCtrl from '../controllers/products.controller';
+import {verifyToken} from '../middlewares';
 
 
-
-router.post('/', productsCtrl.createProduct)
-router.get('/', productsCtrl.getProducts)
-router.get('/:productId', productsCtrl.getProductById)
-router.put('/:productId', productsCtrl.updateProductById)
-router.delete('/:productId', productsCtrl.deleteProductById)
+router.post("/",verifyToken, productsCtrl.createProduct);
+router.get('/', productsCtrl.getProducts);
+router.get('/:productId', productsCtrl.getProductById);
+router.put('/:productId',verifyToken, productsCtrl.updateProductById);
+router.delete('/:productId',verifyToken, productsCtrl.deleteProductById);
 
 export default router;
